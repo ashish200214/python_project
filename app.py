4a3d52ddee18f3fe37ca8666102731a6fe60ddc2
Ubuntu <ubuntu@ip-172-31-23-62.ec2.internal>
2026-01-07 17:09:51 +0000

    changes

--- app.py
+++ app.py
@@ -2,26 +2,9 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
+@app.route("/")
 def home():
-    return jsonify({
-        "status": "success",
-        "message": "🚀 REST API is running"
-    })
-
-@app.route("/health", methods=["GET"])
-def health():
-    return jsonify({
-        "service": "python-rest-api",
-        "status": "UP"
-    })
-
-@app.route("/user/<name>", methods=["GET"])
-def user(name):
-    return jsonify({
-        "username": name,
-        "role": "tester"
-    })
+    return jsonify({"message": "App is running"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
